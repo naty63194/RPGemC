1022bb6238d13edff04d5fc94e087c9e3faff9c2
Gustavo neves <gustavonpr19@gmail.com>
2023-10-16 10:24:05 -0300

    navegando.c

---
+++ navegando.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+// Criação do função navegando
+void navegando(){
+	// Variável que foi usada no comando for
+	int  i;
+	for (i =0 ; i<12; i++){
+		printf("NAVEGANDO...\n");
+	// Comando sleep() foi usado para definir a duração do laço de repetição (1 segundo)
+		sleep(1);
+	}
+}
